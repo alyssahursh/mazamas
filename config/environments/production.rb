49d6c5a8ba06fb5c64d1863863240e7da362e26b
Alyssa Hursh <akhursh@gmail.com>
2017-01-06 17:48:36 -0800

    Moved email configuration to mail.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,16 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Mandrill Production
-  config.action_mailer.smtp_settings = {
-    address: ENV.fetch("SMTP_ADDRESS"),
-    authentication: :plain
-    domain: ENV.fetch("SMTP_DOMAIN"),
-    enable_starttls_auto: true,
-    password: ENV.fetch("SMTP_PASSWORD"),
-    port: "587",
-    user_name: ENV.fetch("SMTP_USERNAME")
-  }
-
   config.action_mailer.default_url_options = { host: ENV["SMTP_DOMAIN"] }
 
 
