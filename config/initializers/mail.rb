a4f908450d41aa072b3811d586ade30896de9b9b
Alyssa Hursh <akhursh@gmail.com>
2017-01-06 17:55:43 -0800

    Fixed semicolon syntax error in mail.rb

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,10 +1,10 @@
 ActionMailer::Base.smtp_settings= {
   address: "smtp.mandrillapp.com",
   port: 587,
-  enable_starttls_auto: true;
+  enable_starttls_auto: true,
   user_name: ENV["Mazamas"],
   password: ENV["SMTP_PASSWORD"],
-  authentication: "login",
+  authentication: "login"
 }
 
 ActionMailer::Base.delivery_method = :smtp
