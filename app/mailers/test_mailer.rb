9db3990c3d5a0230ac63b2df6cc923ef0fae5cbc
Alyssa Hursh <akhursh@gmail.com>
2017-01-06 17:49:45 -0800

    Added file for test mailer

---
+++ app/mailers/test_mailer.rb
@@ -0,0 +1,13 @@
+class TestMailer < BaseMandrillMailer
+  def test(user_email)
+
+    subject = "OH MY FUCKING GOD YOU DID IT!"
+    merge_vars = {
+      "FIRST_NAME" => "Alyssa",
+      "USER_URL" => "www.google.com",
+    }
+    body = mandrill_template("welcome-email", merge_vars)
+
+    send_mail(user_email, subject, body)
+  end
+end
