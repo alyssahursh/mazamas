3dfdbf1177c551fa488f83d92968b4970017aedf
Alyssa Hursh <akhursh@gmail.com>
2017-01-07 16:32:41 -0800

    Corrected username environment variable

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.smtp_settings= {
   address: "smtp.mandrillapp.com",
   port: 587,
   enable_starttls_auto: true,
-  user_name: ENV["Mazamas"],
+  user_name: ENV["SMTP_USERNAME"],
   password: ENV["SMTP_PASSWORD"],
   authentication: "login"
 }
