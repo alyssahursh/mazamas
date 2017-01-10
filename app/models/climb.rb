a9673efabd5254473c37643bdd6a421ad3b52a99
Alyssa Hursh <akhursh@gmail.com>
2017-01-09 19:41:13 -0800

    Fixed irregular inflector in climb model

--- app/models/climb.rb
+++ app/models/climb.rb
@@ -1,6 +1,6 @@
 class Climb < ActiveRecord::Base
   has_many                  :climb_registrations
-  has_and_belongs_to_many   :climb_grad_emphasis
+  has_and_belongs_to_many   :climb_grad_emphases
   has_and_belongs_to_many   :climb_tags
   belongs_to                :climb_schedule
   has_one                   :specific_date
