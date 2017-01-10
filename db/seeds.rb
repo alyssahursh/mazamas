83e48cad13980e667aa67b8f6bffe134e4f4f159
Alyssa Hursh <akhursh@gmail.com>
2017-01-09 22:48:50 -0800

    Updated ClimbClasses seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,7 @@ EducationProgram.create(abbreviation: "MFA",  name: "Mountaineering First Aid", 
 EducationProgram.create(abbreviation: "AR",   name: "Advanced Rock",                    description: "")
 EducationProgram.create(abbreviation: "ASI",  name: "Advanced Snow and Ice",            description: "")
 EducationProgram.create(abbreviation: "SM",   name: "Ski Mountaineering",               description: "")
+EducationProgram.create(abbreviation: "FM",   name: "Families Mountaineering",          description: "")
 
 # All User Roles
 UserRole.create(role: "Climber",              description: "General role for all climbers")
@@ -23,9 +24,23 @@ UserRole.create(role: "Provisional Leader",   description: "Provisional leader; 
 UserRole.create(role: "Leader Candidate",     description: "Leadership development candidate")
 UserRole.create(role: "Admin",                description: "Adminstration role for staff and volunteers")
 
-# All ClimbClasses
+# All ClimbClasses (RouteGrades)
+ClimbClass.create(code: "A", description: "May require off-trail hiking, ice axe and crampon use on moderately steep slopes, and self-arrest. (examples: Mt. St. Helens, Mt Adams)")
+ClimbClass.create(code: "B", description: "May require off-trail hiking, ice axe and crampon use on moderately steep slopes, self-arrest, as well as glacier travel, and roped climbing. (examples: Mt. Hood South Side, Middle Sister)")
+ClimbClass.create(code: "C", description: "May require off-trail hiking, ice axe and crampon use on moderately steep slopes, self-arrest, as well as glacier travel, roped climbing, crevasse rescue skills, belaying, rappelling, and low 5th class rock climbing. (examples: DC and Emmons on Mt. Rainier, Mt Washington in Oregon)")
+ClimbClass.create(code: "D", description: "May require off-trail hiking, ice axe and crampon use on moderately steep slopes, self-arrest, as well as glacier travel, roped climbing, crevasse rescue skills, belaying, rappelling, with potential travel on steep snow slopes or 5th class rock climbing. (examples: West Ridge of Mt. Stuart, Sandy Glacier Headwall on Mt Hood)")
+ClimbClass.create(code: "E", description: "Multi-pitch technical routes with high degree of self-sufficiency and proficiency using technical skills under high exposure, sharing high angle leads, setting protection and anchors.  (examples: North ridge of Mt. Stuart, Beckey Route, Liberty Bell)")
 
 # All ClimbTags
+ClimbTag.create(code: "Base Camp",                  description: "Multi-day climb with pack-in to base camp.")
+ClimbTag.create(code: "Classic Climbers",           description: "Climbers 55 years of age and older are given preference")
+ClimbTag.create(code: "Exploratory",                description: "The leader has not previously climbed this route")
+ClimbTag.create(code: "Family",                     description: "Kid friendly climb at a moderate pace")
+ClimbTag.create(code: "Hike to the Summit",         description: "Nonmembers given preference for glaciated peak climb")
+ClimbTag.create(code: "Linked Climbs",              description: "Two or more climbs from the same base camp. Climbers must apply to all related climbs")
+ClimbTag.create(code: "Adjacent Climbs",            description: "Two or more climbs in a similar area. Climbers may apply to one or all.")
+ClimbTag.create(code: "Ski Mountaineering",         description: "Backcountry skie or snowboard gear required")
+ClimbTag.create(code: "Adventurous Young Mazamas",  description: "For those young or young at heart")
 
 # Sample Climb Schedules
 ClimbSchedule.create(season: "Winter", year: 2016-2017)
