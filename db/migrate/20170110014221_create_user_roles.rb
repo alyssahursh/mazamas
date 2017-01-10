36b650d72edde51caf2a7cb8f1aa2d6af534d3ca
Alyssa Hursh <akhursh@gmail.com>
2017-01-09 18:14:47 -0800

    Generated user and user roles models, and migrated database

---
+++ db/migrate/20170110014221_create_user_roles.rb
@@ -0,0 +1,10 @@
+class CreateUserRoles < ActiveRecord::Migration
+  def change
+    create_table :user_roles do |t|
+      t.string :role
+      t.string :description
+
+      t.timestamps null: false
+    end
+  end
+end
