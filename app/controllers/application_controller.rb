8f44e7264501085485e8eac0ee9cce57c1c0a2bb
Alyssa Hursh <akhursh@gmail.com>
2017-01-23 17:50:54 -0800

    Added phone number converter method to app controller helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,15 @@ module ApplicationHelper
   def title(text)
     content_for :title, text
   end
+
+  def phone_convert(phone_number)
+    phone_number = phone_number.gsub(/\D/, '')
+    length = phone_number.length
+
+    if length == 10
+      return number_to_phone(phone_number, area_code: true)
+    else
+      return number_to_phone(phone_number[0...10], area_code: true, extension: phone_number[10..-1])
+    end
+  end
 end
